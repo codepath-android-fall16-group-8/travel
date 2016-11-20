17a522b24b5321a284a464224ca8aa2877359ff4
Aditi Kakade <adi.kakade31@gmail.com>
2016-11-19 20:09:36 -0800

    in progress

---
+++ app/src/main/java/com/codepath/travel/callbacks/ImageUploadCallback.java
@@ -0,0 +1,12 @@
+package com.codepath.travel.callbacks;
+
+import com.parse.ParseException;
+
+/**
+ * Created by rpraveen on 11/19/16.
+ */
+
+public interface ImageUploadCallback {
+    void onImageUploadSuccess(String uploadedImageURL);
+    void onImageUploadError(ParseException imageUploadError);
+}
