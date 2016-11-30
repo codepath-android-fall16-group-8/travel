508a8100b7a3819a0897cba8add4c0686e492cd6
Aditi Kakade <adi.kakade31@gmail.com>
2016-11-30 14:38:58 -0800

    Issue83: Custom autocomplete fragment styling

---
+++ app/src/main/java/com/codepath/travel/fragments/CustomPlaceAutoCompleteFragment.java
@@ -0,0 +1,132 @@
+package com.codepath.travel.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.codepath.travel.R;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+/**
+ * Created by aditikakadebansal on 11/30/16.
+ */
+
+public class CustomPlaceAutoCompleteFragment extends PlaceAutocompleteFragment {
+
+    private EditText etSearch;
+
+    private View zzaRh;
+    private View zzaRi;
+    private EditText zzaRj;
+    @Nullable
+    private LatLngBounds zzaRk;
+    @Nullable
+    private AutocompleteFilter zzaRl;
+    @Nullable
+    private PlaceSelectionListener zzaRm;
+
+    public CustomPlaceAutoCompleteFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View var4 = inflater.inflate(R.layout.layout_place_autocomplete, container, false);
+
+        etSearch = (EditText) var4.findViewById(R.id.etAutocomplete);
+        etSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zzzG();
+            }
+        });
+
+        return var4;
+    }
+    public void onDestroyView() {
+        this.zzaRh = null;
+        this.zzaRi = null;
+        this.etSearch = null;
+        super.onDestroyView();
+    }
+
+    public void setBoundsBias(@Nullable LatLngBounds bounds) {
+        this.zzaRk = bounds;
+    }
+
+    public void setFilter(@Nullable AutocompleteFilter filter) {
+        this.zzaRl = filter;
+    }
+
+    public void setText(CharSequence text) {
+        this.etSearch.setText(text);
+        //this.zzzF();
+    }
+
+    public void setHint(CharSequence hint) {
+        this.etSearch.setHint(hint);
+        this.zzaRh.setContentDescription(hint);
+    }
+
+    public void setOnPlaceSelectedListener(PlaceSelectionListener listener) {
+        this.zzaRm = listener;
+    }
+    private void zzzF() {
+        boolean var1 = !this.etSearch.getText().toString().isEmpty();
+        //this.zzaRi.setVisibility(var1?0:8);
+    }
+
+    private void zzzG() {
+        int var1 = -1;
+
+        try {
+            Intent var2 = (new PlaceAutocomplete.IntentBuilder(2)).setBoundsBias(this.zzaRk).setFilter(this.zzaRl).zzlf(this.etSearch.getText().toString()).zzwg(1).build(this.getActivity());
+            this.startActivityForResult(var2, 1);
+        } catch (GooglePlayServicesRepairableException var3) {
+            var1 = var3.getConnectionStatusCode();
+            Log.e("Places", "Could not open autocomplete activity", var3);
+        } catch (GooglePlayServicesNotAvailableException var4) {
+            var1 = var4.errorCode;
+            Log.e("Places", "Could not open autocomplete activity", var4);
+        }
+
+        if (var1 != -1) {
+            GoogleApiAvailability var5 = GoogleApiAvailability.getInstance();
+            var5.showErrorDialogFragment(this.getActivity(), var1, 2);
+        }
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == -1) {
+                Place var4 = PlaceAutocomplete.getPlace(this.getActivity(), data);
+                if (this.zzaRm != null) {
+                    this.zzaRm.onPlaceSelected(var4);
+                }
+
+                this.setText(var4.getName().toString());
+            } else if (resultCode == 2) {
+                Status var5 = PlaceAutocomplete.getStatus(this.getActivity(), data);
+                if (this.zzaRm != null) {
+                    this.zzaRm.onError(var5);
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
